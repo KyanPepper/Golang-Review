0baecb7d9ae8b20ee0843717c065dd07d2f5f96f
kyanpepper <kyankks1@gmail.com>
2024-06-26 15:22:58 -0700

    Simple bst finished

--- BST/bst.go
+++ BST/bst.go
@@ -5,14 +5,13 @@ import (
 )
 
 type Node struct {
-	data int 
-	left *Node
+	data  int
+	left  *Node
 	right *Node
 }
 
-
 func NodeFactory(data int) *Node {
-	createdNode := new(Node);
+	createdNode := new(Node)
 	createdNode.data = data
 	createdNode.left = nil
 	createdNode.right = nil
@@ -20,3 +19,28 @@ func NodeFactory(data int) *Node {
 	fmt.Println(data)
 	return createdNode
 }
+
+func insert(data int, root *Node) bool {
+	//look left
+	if data < root.data {
+		if root.left != nil {
+			insert(data, root.left)
+		} else {
+			root.left = NodeFactory(data)
+		}
+		return true
+	}
+
+	//look right 
+	if data > root.data {
+		if root.right != nil {
+			insert(data, root.left)
+		} else {
+			root.right = NodeFactory(data)
+		}
+		return false
+	}
+
+	return true
+}
+
--- BST/main.go
+++ BST/main.go
@@ -5,7 +5,14 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, world!")
 	root := NodeFactory(8)
+	insert(6,root)
+	insert(9,root)
+	insert(7,root)
+	fmt.Println("Expected output 6,7,8,9")
 	
+	fmt.Println("actual: " )
+	fmt.Printf("%d %d %d %d", root.left.data, root.left.right.data, root.data, root.right.data )
+
 }
 
 
